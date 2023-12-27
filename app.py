987c0f50215b6938428034e136c8d85f74925ae5
corina <fabienne0717@gapp.nthu.edu.tw>
2023-12-27 22:39:36 +0800

    test api

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+import json
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
+@app.route("/get-data")
+def get_data():
+    headers = {'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdyb3VwMSIsInV1aWQiOiJlYTcyNDI0MC04OTI2LTQwMTAtYjljZS1mNzM0YzE1ZmNjN2IiLCJuYW1lIjoiZ3JvdXAxIiwiaWF0IjoxNzAzNjc4NTY3LCJleHAiOjE3MDM3NjQ5Njd9._pOXTF6RmiBWMZrloLwCqquTSS4zT5n6NCaIJLvIhx0',
+    'Content-Type': 'application/json'}
+    response = request.get('https://smart-campus.kits.tw/api/api/sensorgroup_in_area/80285c45-d917-4ee7-9a3d-299d4495a181', headers=headers)
+    return response.text
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -24,7 +31,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    # message = get_data()
+    line_bot_api.reply_message(event.reply_token, get_data())
 
 import os
 if __name__ == "__main__":
