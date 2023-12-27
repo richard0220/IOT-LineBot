5ee6c171509f1eb5cb477c0fc7b18f30eb95fa68
corina <fabienne0717@gapp.nthu.edu.tw>
2023-12-27 22:57:20 +0800

    test api

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
-@app.route("/")
 def get_data():
     headers = {'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdyb3VwMSIsInV1aWQiOiJlYTcyNDI0MC04OTI2LTQwMTAtYjljZS1mNzM0YzE1ZmNjN2IiLCJuYW1lIjoiZ3JvdXAxIiwiaWF0IjoxNzAzNjc4NTY3LCJleHAiOjE3MDM3NjQ5Njd9._pOXTF6RmiBWMZrloLwCqquTSS4zT5n6NCaIJLvIhx0',
     'Content-Type': 'application/json'}
     # response = request.get('https://smart-campus.kits.tw/api/api/sensors/BATTERY_VOLTAGE/8fd928dc-b2b1-4efd-a5d1-8087f62bb0ab', headers=headers)
     response = "I'm API :D"
+    return response
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -32,7 +32,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     # message = get_data()
-    line_bot_api.reply_message(event.reply_token, response)
+    line_bot_api.reply_message(event.reply_token, get_data())
 
 import os
 if __name__ == "__main__":
