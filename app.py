18953cd0bcdf33f4ed331337f781cb3b5550027f
corina <fabienne0717@gapp.nthu.edu.tw>
2023-12-27 23:28:08 +0800

    test api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, render_template
+from flask import Flask, request, abort, render_template
 import requests
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -31,15 +31,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # headers = {'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdyb3VwMSIsInV1aWQiOiJlYTcyNDI0MC04OTI2LTQwMTAtYjljZS1mNzM0YzE1ZmNjN2IiLCJuYW1lIjoiZ3JvdXAxIiwiaWF0IjoxNzAzNjc4NTY3LCJleHAiOjE3MDM3NjQ5Njd9._pOXTF6RmiBWMZrloLwCqquTSS4zT5n6NCaIJLvIhx0',
-    # 'Content-Type': 'application/json'}
-    # response = requests.get(url='https://smart-campus.kits.tw/api/api/sensors/BATTERY_VOLTAGE/8fd928dc-b2b1-4efd-a5d1-8087f62bb0ab', headers=headers)
+    headers = {'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdyb3VwMSIsInV1aWQiOiJlYTcyNDI0MC04OTI2LTQwMTAtYjljZS1mNzM0YzE1ZmNjN2IiLCJuYW1lIjoiZ3JvdXAxIiwiaWF0IjoxNzAzNjc4NTY3LCJleHAiOjE3MDM3NjQ5Njd9._pOXTF6RmiBWMZrloLwCqquTSS4zT5n6NCaIJLvIhx0',
+    'Content-Type': 'application/json'}
+    response = requests.get(url='https://smart-campus.kits.tw/api/api/sensors/BATTERY_VOLTAGE/8fd928dc-b2b1-4efd-a5d1-8087f62bb0ab', headers=headers)
     # sensorId = response.get('sensorId', None) # 解析資料，若不是 JSON，則返回 None
     # value = response.get('value', None)
     # result = f'Hello {sensorId},{value}'
 
-    message = TextSendMessage(text=event.message.text)
-    # message = TextSendMessage(text=response.text)
+    # message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text=response.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
