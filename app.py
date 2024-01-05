7023b16bdf1e32f905bb830c7414fdff50e57f10
Richard <richard0311312@gmail.com>
2024-01-05 23:07:27 +0800

    change reply state

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate
+from linebot.models import PostbackAction,URIAction, MessageAction, TemplateSendMessage, ButtonsTemplate, LocationSendMessage, MessageTemplateAction
 import os
 import json
 
@@ -32,48 +32,54 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_token = "https://smart-campus.kits.tw/api/api/account/login"
-    data = {'email':'bachelor_07',
-            'password':'bachelor_07'}
-    response = requests.post(get_token, data)
-    token = response.json()["token"] #'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJhY2hlbG9yXzA3IiwidXVpZCI6ImFiZDMyMjhkLThmNWQtNDJmMS1hODY4LWIwODA0OTUzMTg1ZiIsIm5hbWUiOiJiYWNoZWxvcl8wNyIsImlhdCI6MTcwNDM2NjM2MiwiZXhwIjoxNzA0NDUyNzYyfQ.tr4e1oeRpQ2Yfzp83XzGb14HY7IeEj8Fhrm9742PGFg'
-    headers = {'token':token,
-               'Content-Type': 'application/json'}
-    # Get GPS data from sensor id
-    sensor_id = "df11640a-35f9-4f2b-abae-0016cfac40ba"
+    if event.message.text == "找機車":
+        # Get the required token
+        get_token = "https://smart-campus.kits.tw/api/api/account/login"
+        data = {'email':'bachelor_07',
+                'password':'bachelor_07'}
+        response = requests.post(get_token, data)
+        token = response.json()["token"]
 
-    get_gps_api = "https://smart-campus.kits.tw/api/api/sensors/GPS/" + sensor_id
+        # Get GPS data from sensor id
+        headers = {'token':token, 
+                   'Content-Type': 'application/json'}
+        sensor_id = "df11640a-35f9-4f2b-abae-0016cfac40ba"
+        get_gps_api = "https://smart-campus.kits.tw/api/api/sensors/GPS/" + sensor_id
 
-    try:
-        response = requests.get(url=get_gps_api, headers=headers)
+        try:
+            response = requests.get(url=get_gps_api, headers=headers)
 
-        gps_data = response.json()[0]["value"]
-        gps_data = hex(gps_data)
+            gps_data = response.json()[-1]["value"]
+            gps_data = hex(gps_data)
 
-        latitude, longtitude = gps_data[2:9], gps_data[9:]
+            latitude, longtitude = gps_data[2:9], gps_data[9:]
 
-        latitude = int(latitude, 16) * (10 ** (-7))
-        longtitude = int(longtitude, 16) * (10 ** (-7))
-        str_latitude = str(latitude)
-        str_longtitude = str(longtitude)
-        LocationLink =  "https://www.google.com/maps/search/?api=1&query="+str_latitude+","+str_longtitude
+            latitude = int(latitude, 16) * (10 ** (-7))
+            longtitude = int(longtitude, 16) * (10 ** (-7))
+            str_latitude = str(latitude)
+            str_longtitude = str(longtitude)
+            LocationLink =  "https://www.google.com/maps/search/?api=1&query="+str_latitude+","+str_longtitude
+            location_message = LocationSendMessage(title="你的機車", address="最後紀錄時間", latitude=latitude, longitude=longtitude)
+            line_bot_api.reply_message(event.reply_token, location_message)
+
+        except requests.exceptions.RequestException as e:
+            message = TextSendMessage(text="Enable to fetch gps from api!!!")
+            line_bot_api.reply_message(event.reply_token, message)
+
+    else:
         buttons_template = ButtonsTemplate(
             title='My Button Template',
             text='Please select an option:',
             actions=[
-                URIAction(
+                MessageTemplateAction(
                     label='找機車',
-                    uri=LocationLink
+                    text='找機車'
                 )
             ]
         )
         # Send the button template as a reply
         template_message = TemplateSendMessage(alt_text='Buttons Template', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
-
-    except requests.exceptions.RequestException as e:
-        message = TextSendMessage(text="Enable to fetch gps from api!!!")
-        line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
